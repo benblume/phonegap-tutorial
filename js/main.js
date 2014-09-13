462481d11a37b0954987f4dc96b12bd537c51756
Ben Blume <ben.blume@gmail.com>
2014-09-13 20:24:48 +0100

    More views and some styling

--- js/main.js
+++ js/main.js
@@ -7,26 +7,11 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},
-	
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-    
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
 
     initialize: function() {
     	var self = this;
-    	this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-		
         this.store = new MemoryStore(function() {
-        	self.renderHomeView();
+        	$('body').html(new HomeView(self.store).render().el);
     	});
     }
 
